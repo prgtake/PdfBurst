70a6e56fe7a1cd92f578a94ac4c59dd7d47587a1
データン <prgtake@gmail.com>
2026-04-22 18:31:14 +0900

    build.batの修正（不要なpytestの読み飛ばし）
    build.batの修正（不要なpytestの読み飛ばし）

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 @echo off
 echo [PdfCrop Build Script]
 echo 必要なライブラリの確認中...
-pip install pymupdf pillow pyinstaller
+pip install --upgrade pymupdf pillow pyinstaller pyinstaller-hooks-contrib
 
 echo.
 echo ビルドを開始します (onefile, noconsole)...
-pyinstaller --onefile --noconsole --name PdfCrop pdf_cropper.py
+pyinstaller --onefile --noconsole --name PdfCrop --exclude-module pytest pdf_cropper.py
 
 echo.
 echo ビルドが完了しました。
